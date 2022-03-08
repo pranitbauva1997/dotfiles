1e902130365c22dc2897ee49a0daab255657e985
Pranit Bauva <pranit.bauva@gmail.com>
2022-03-09 00:31:38 +0530

    arch linux: clean up packages

--- setup-arch-linux.sh
+++ setup-arch-linux.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm vim cmake aria2 \
+sudo pacman -S vim cmake aria2 \
     a52dec faac faad2 flac jasper lame libdca \
     libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 \
     xvidcore vlc p7zip \
     mythes-en gst-plugins-good ttf-droid jdk11-openjdk icedtea-web \
     ttf-ubuntu-font-family ttf-anonymous-pro ufw \
     wavpack libdv libmad gst-libav libdvdnav fuse-exfat libvorbis faac \
-    libdvdcss lame libmpeg2 libtheora libxv libdvdread exfat-utils \
-    a52dec faad2 flac jasper gstreamer \
-    deja-dup
+    libdvdcss lame libmpeg2 libtheora libxv libdvdread \
+    gstreamer deja-dup
 
 # Final manual steps
 # set GRUB_TIMEOUT=0; GRUB_HIDDEN_TIMEOUT=0; GRUB_DEFAULT=saved
