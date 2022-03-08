d3bf437b2a15b11fdfc5887585300a205316339b
Pranit Bauva <pranit.bauva@gmail.com>
2022-03-09 00:20:18 +0530

    update arch linux setup to remove packages

--- setup-arch-linux.sh
+++ setup-arch-linux.sh
@@ -1,12 +1,12 @@
 sudo pacman -S --noconfirm vim cmake aria2 \
     a52dec faac faad2 flac jasper lame libdca \
     libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 \
-    xvidcore gstreamer0.10-plugins vlc p7zip p7zip-plugins update-grub \
+    xvidcore vlc p7zip \
     mythes-en gst-plugins-good ttf-droid jdk11-openjdk icedtea-web \
     ttf-ubuntu-font-family ttf-anonymous-pro ufw \
     wavpack libdv libmad gst-libav libdvdnav fuse-exfat libvorbis faac \
     libdvdcss lame libmpeg2 libtheora libxv libdvdread exfat-utils \
-    a52dec faad2 flac jasper gstreamer chromium visual-studio-code \
+    a52dec faad2 flac jasper gstreamer chromium \
     deja-dup
 
 # Final manual steps
@@ -15,6 +15,4 @@ sudo pacman -S --noconfirm vim cmake aria2 \
 
 # Remove orphans
 sudo pacman -Rns $(pacman -Qtdq)
-
-sudo pacman-optimize
 
