b703a333e1ee45d987f171e9171bf14e4b35c5f9
Pranit Bauva <pranit@bauva.com>
2024-01-10 01:43:17 +0530

    tmux configs

---
+++ bin/nirah-code-tmux.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+SESSIONNAME="nirah-code"
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ] 
+ then
+    tmux new-session -s $SESSIONNAME -n script -d
+
+    tmux rename-window -t $SESSIONNAME:0 git
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/core" C-m 
+    tmux split-window -h -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/nomad-experiments" C-m 
+    tmux split-window -v -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/website" C-m 
+    tmux select-pane -L
+    tmux split-window -v -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/dashboard" C-m 
+
+    tmux new-window -t $SESSIONNAME -n core
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/core" C-m 
+    tmux split-window -h -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/core" C-m 
+    tmux split-window -v -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/core" C-m 
+    tmux select-pane -L
+    tmux split-window -v -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/core" C-m 
+
+    tmux new-window -t $SESSIONNAME -n dashboard
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/dashboard" C-m 
+    tmux split-window -h -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/dashboard" C-m 
+    tmux split-window -v -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/dashboard" C-m 
+    tmux select-pane -L
+    tmux split-window -v -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/dashboard" C-m 
+
+    tmux new-window -t $SESSIONNAME -n infra
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/nomad-experiments" C-m 
+    tmux split-window -h -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/nomad-experiments" C-m 
+    tmux split-window -v -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/nomad-experiments" C-m 
+    tmux select-pane -L
+    tmux split-window -v -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/nomad-experiments" C-m 
+
+    tmux new-window -t $SESSIONNAME -n website
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/website" C-m 
+    tmux split-window -h -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/website" C-m 
+    tmux split-window -v -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/website" C-m 
+    tmux select-pane -L
+    tmux split-window -v -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "cd ~/Codes/nirahapp/website" C-m 
+fi
+
+tmux attach -t $SESSIONNAME
+
