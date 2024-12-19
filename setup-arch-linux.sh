1cf8682e55d0d680aec229c79209b7687a5b98da
Pranit Bauva <pranit@bauva.com>
2024-12-19 20:00:38 +0530

    python data science packages

--- setup-arch-linux.sh
+++ setup-arch-linux.sh
@@ -32,6 +32,8 @@ sudo pacman -S --noconfirm traceroute nmap wireshark-qt
 # Python
 sudo pacman -S --noconfirm python-virtualenv python-pip
 
+sudo pacman -S python-numpy python-scipy python-matplotlib python-sympy jupyterlab python-pandas python-pandas-datareader python-numexpr python-bottleneck python-tabulate python-scikit-learn
+
 # Golang
 sudo pacman -S --noconfirm go go-tools
 
