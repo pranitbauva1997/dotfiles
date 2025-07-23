6f7129829776f81e2a87d2adeda41d6b5247fb58
Pranit Bauva <pranit@bauva.com>
2025-07-23 16:23:00 +0530

    some more python packages

--- setup-arch-linux.sh
+++ setup-arch-linux.sh
@@ -55,12 +55,12 @@ sudo pacman -S --noconfirm python-pipx python-numpy python-scipy python-matplotl
     python-unicodedata2 python-uharfbuzz python-tzdata python-dask python-distributed \
     python-email-validator python-graphviz python-jax python-jaxlib python-flax \
     python-pyaudio python-simpleaudio python-paddlepaddle python-pynvim \
-    python-tenacity
+    python-tenacity python-prompt_toolkit bpython python-click python-typer
 
 yay -S --noconfirm python-blobfile python-google-genai python-anthropic \
     python-dspy python-mistralai python-rq python-apscheduler python-llm \
     python-llm-openrouter python-llm-ollama python-pydeck python-weaviate-client \
-    python-uharfbuzz python-gradio
+    python-uharfbuzz python-gradio python-questionary
 
 yay -S --noconfirm python-hf-transfer python-safetensors python-pycocotools
 
