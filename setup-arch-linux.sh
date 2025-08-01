3c451a5374a08baa545ab089180b1ed15c62ad8d
Pranit Bauva <pranit@bauva.com>
2025-08-01 16:16:37 +0530

    some more python libs

--- setup-arch-linux.sh
+++ setup-arch-linux.sh
@@ -55,7 +55,8 @@ sudo pacman -S --noconfirm python-pipx python-numpy python-scipy python-matplotl
     python-unicodedata2 python-uharfbuzz python-tzdata python-dask python-distributed \
     python-email-validator python-graphviz python-jax python-jaxlib python-flax \
     python-pyaudio python-simpleaudio python-paddlepaddle python-pynvim \
-    python-tenacity python-prompt_toolkit bpython python-click python-typer
+    python-tenacity python-prompt_toolkit bpython python-click python-typer \
+    python-loguru python-pyaudio
 
 yay -S --noconfirm python-blobfile python-google-genai python-anthropic \
     python-dspy python-mistralai python-rq python-apscheduler python-llm \
