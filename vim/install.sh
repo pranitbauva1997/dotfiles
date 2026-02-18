df173e72fa5e53700befce42cbb6b2003e1a3587
Pranit Bauva <pranit@bauva.com>
2026-02-19 01:34:07 +0530

    Refactor vim/install.sh to use symlinks and cleaner plugin loop

--- vim/install.sh
+++ vim/install.sh
@@ -3,114 +3,46 @@
 VIM_DIR=${HOME}/.vim
 VIM_PLUGINS=${VIM_DIR}/pack/vendor/start
 PLUGIN_OPTS="--depth 1 --branch master"
+DOTFILES_VIM_RC="${HOME}/Codes/dotfiles/vim/.vimrc"
 
-write_vimrc() {
-  cat > "${HOME}/.vimrc" <<EOF
-filetype on
-filetype plugin indent on
-set encoding=utf-8
-set fileencoding=utf-8
-set number
-set relativenumber
-set expandtab
-set shiftwidth=4
-set softtabstop=4
-set tabstop=4
-set autoindent
-syntax enable
-set lazyredraw
-set showmatch
-set spell
-set hlsearch
-colorscheme pablo
-let mapleader = ','
-
-" Clipboard and Keymaps
-set clipboard=unnamedplus
-vnoremap <C-c> "+y
-
-" Set custom indent width for particular filetype
-autocmd Filetype html set shiftwidth=2 softtabstop=2 tabstop=2
-autocmd Filetype css set shiftwidth=2 softtabstop=2 tabstop=2
-autocmd Filetype javascript set shiftwidth=2 softtabstop=2 tabstop=2
-autocmd Filetype javascriptreact set shiftwidth=2 softtabstop=2 tabstop=2
-autocmd Filetype typescript set shiftwidth=2 softtabstop=2 tabstop=2
-autocmd Filetype typescriptcommon set shiftwidth=2 softtabstop=2 tabstop=2
-autocmd Filetype typescriptreact set shiftwidth=2 softtabstop=2 tabstop=2
-autocmd Filetype json set shiftwidth=2 softtabstop=2 tabstop=2
-
-autocmd Filetype make set noexpandtab
-
-" Set spell check for markdown and text file
-autocmd Filetype markdown set spell
-autocmd Filetype markdown_tablemode set spell
-autocmd Filetype text set spell
-
-" Maintain undo history between sessions
-set undofile
-set undodir=~/.vim/undodir
-
-" Set folding
-set foldmethod=indent
-
-set backspace=indent,eol,start
-
-" Language
-autocmd BufNewFile,BufRead *.jsx set filetype=javascript.jsx
-autocmd BufNewFile,BufRead *.tsx set filetype=typescript.tsx
-
-" Plugin Configurations
-"
-" Vim fugitive
-noremap <Leader>gd :Git diff<CR>
-noremap <Leader>gl :Git log<CR>
-noremap <Leader>gb :Git blame<CR>
-
-" Vim Gutter / Sync settings
-set updatetime=250
-
-" Remember folds
-augroup remember_folds
-  autocmd!
-  autocmd BufWinLeave * mkview
-  autocmd BufWinEnter * silent! loadview
-augroup END
-
-let python_highlight_all = 1
-
-" NERDTree Mappings
-noremap <Leader>n :NERDTreeToggle<CR>
-
-" Tags
-noremap <C-w>[ :vsp <CR>:exec("tag ".expand("<cword>"))<CR>
-EOF
+link_vimrc() {
+  echo "Linking .vimrc from dotfiles..."
+  ln -sf "${DOTFILES_VIM_RC}" "${HOME}/.vimrc"
 }
-
 
 # Install Plugins (Optimized for agile viewing)
 vim_plugins() {
+  echo "Installing/Resetting plugins..."
   rm -rf "${VIM_PLUGINS}"
   mkdir -p "${VIM_PLUGINS}"
   cd "${VIM_PLUGINS}" || exit
-  eval "git clone ${PLUGIN_OPTS} https://github.com/ctrlpvim/ctrlp.vim.git"
-  eval "git clone ${PLUGIN_OPTS} https://github.com/preservim/nerdtree.git"
-  eval "git clone ${PLUGIN_OPTS} https://github.com/tpope/vim-surround.git"
-  eval "git clone ${PLUGIN_OPTS} https://github.com/tpope/vim-fugitive.git"
-  eval "git clone ${PLUGIN_OPTS} https://github.com/vim-airline/vim-airline.git"
-  eval "git clone ${PLUGIN_OPTS} https://github.com/tomtom/tcomment_vim.git"
-  eval "git clone ${PLUGIN_OPTS} https://github.com/hdima/python-syntax.git"
-  eval "git clone ${PLUGIN_OPTS} https://github.com/Xuyuanp/nerdtree-git-plugin.git"
-  eval "git clone ${PLUGIN_OPTS} https://github.com/jeffkreeftmeijer/vim-numbertoggle.git"
-  eval "git clone ${PLUGIN_OPTS} https://github.com/ervandew/supertab.git"
+  
+  plugins="
+    https://github.com/ctrlpvim/ctrlp.vim.git
+    https://github.com/preservim/nerdtree.git
+    https://github.com/tpope/vim-surround.git
+    https://github.com/tpope/vim-fugitive.git
+    https://github.com/vim-airline/vim-airline.git
+    https://github.com/tomtom/tcomment_vim.git
+    https://github.com/hdima/python-syntax.git
+    https://github.com/Xuyuanp/nerdtree-git-plugin.git
+    https://github.com/jeffkreeftmeijer/vim-numbertoggle.git
+    https://github.com/ervandew/supertab.git
+  "
+
+  for plugin in $plugins; do
+    echo "Cloning $plugin..."
+    git clone ${PLUGIN_OPTS} "$plugin"
+  done
 }
 
-
 all() {
-  vim_plugins && write_vimrc
+  vim_plugins && link_vimrc
 }
 
 case "$1" in
   --all ) all ; exit; ;;
-  --vimrc-only ) write_vimrc ; exit; ;;
-  * ) echo "invalid argument: $1" ; exit; ;;
+  --plugins-only ) vim_plugins ; exit; ;;
+  --link-only ) link_vimrc ; exit; ;;
+  * ) echo "Usage: $0 {--all|--plugins-only|--link-only}" ; exit 1; ;;
 esac
